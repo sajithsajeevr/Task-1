d9d636dfdba0d8dce2a6f89540dc1a544ff7d215
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-30 04:40:11 +0530

    fname lastname pair as object

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 
 document.querySelector('form').addEventListener('submit',function(e){
 console.log('readyy');
-const name=document.getElementById('name').value;
-console.log(name);
-localStorage.setItem('name',name)
+const fname=document.getElementById('fname').value;
+const lname=document.getElementById('lname').value;
+console.log(fname);
+let peoples;
+let name =new people(fname,lname);
+console.log(a.firstname);
+//if(localStorage.getItem('peop'))
+//localStorage.setItem('fname',fname)
 e.preventDefault();
 })//should add firstname and lastname as a single object
+
+
+
+function people(fname,lname){
+    this.firstname=fname;
+    this.lastname=lname;
+}
