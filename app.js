19c4b30c8ce80d435592d76d71acea8b8a64b7a5
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-30 10:45:54 +0530

    retrieving firstname and last name, appending as list

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const lname=document.getElementById('lname').value;
 console.log(fname);
 let name=`${fname}+${lname}`;
 let users;
-//localStorage.setItem('users',JSON.stringify(name));
-// let name =new people(fname,lname);
-// console.log(name.firstname);
-// console.log(name.lastname);
 if(localStorage.getItem('users')==null){
  users=[];
  console.log(users);}
@@ -18,17 +14,6 @@ if(localStorage.getItem('users')==null){
 }
  console.log(users)
 users.push(name);
-//console.log(users)
  localStorage.setItem('users',JSON.stringify(users));
-
-//if(localStorage.getItem('peop'))
-//localStorage.setItem('fname',fname)
 e.preventDefault();
-})//should add firstname and lastname as a single object
-
-
-
-// function people(fname,lname){
-//     this.firstname=fname;
-//     this.lastname=lname;
-// }
+})
---
+++ usersapp.js
@@ -0,0 +1,8 @@
+let users=JSON.parse(localStorage.getItem('users'));
+console.log(users);
+users.forEach(element => {
+let user=element.split('+');
+console.log(user[0]);
+document.getElementById('users').innerHTML+=`<li>${user[0]} ${user[1]}</li>`;
+    
+});
