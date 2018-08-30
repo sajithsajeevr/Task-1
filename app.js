d268e1ad5286128091d39c527222114f521a0a6c
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-30 10:17:36 +0530

    storing name in localstorage working

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ console.log('readyy');
 const fname=document.getElementById('fname').value;
 const lname=document.getElementById('lname').value;
 console.log(fname);
-let peoples;
-let name =new people(fname,lname);
-console.log(a.firstname);
+let name=`${fname}+${lname}`;
+let users;
+//localStorage.setItem('users',JSON.stringify(name));
+// let name =new people(fname,lname);
+// console.log(name.firstname);
+// console.log(name.lastname);
+if(localStorage.getItem('users')==null){
+ users=[];
+ console.log(users);}
+ else{
+   users=JSON.parse(localStorage.getItem('users'));
+}
+ console.log(users)
+users.push(name);
+//console.log(users)
+ localStorage.setItem('users',JSON.stringify(users));
+
 //if(localStorage.getItem('peop'))
 //localStorage.setItem('fname',fname)
 e.preventDefault();
@@ -14,7 +28,7 @@ e.preventDefault();
 
 
 
-function people(fname,lname){
-    this.firstname=fname;
-    this.lastname=lname;
-}
+// function people(fname,lname){
+//     this.firstname=fname;
+//     this.lastname=lname;
+// }
