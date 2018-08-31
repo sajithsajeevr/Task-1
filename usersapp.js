46425b4ba4faca05b168ab63a060cd30a1ce976c
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-31 19:16:12 +0530

    clear button in user list added

--- usersapp.js
+++ usersapp.js
@@ -1,3 +1,9 @@
+
+
+if(localStorage.getItem('persistingusersls')==null)
+{
+    document.getElementById('users').innerHTML+=`<li class="collection-item">No Users Found</li>`;
+}else{
 let users=JSON.parse(localStorage.getItem('persistingusersls'));
 console.log(users);
 users.forEach(element => {
@@ -5,4 +11,12 @@ let user=element.split('+');
 console.log(user[0]);
 document.getElementById('users').innerHTML+=`<li class="collection-item">${user[0]} ${user[1]}</li>`;
     
-});
+});}
+
+document.getElementById('clearUsers').addEventListener('click',function(e){
+localStorage.clear();
+console.log("clear");
+if(localStorage.getItem('persistingusersls')==null){
+    console.log("empty");
+    document.getElementById('users').innerHTML=`<li class="collection-item">No Users Found</li>`;
+}else{}});
