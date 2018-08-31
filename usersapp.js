ecd4bd241d0f39667e6de25040024806306abdf3
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-31 16:31:14 +0530

    materialize css in both pages

--- usersapp.js
+++ usersapp.js
@@ -3,6 +3,6 @@ console.log(users);
 users.forEach(element => {
 let user=element.split('+');
 console.log(user[0]);
-document.getElementById('users').innerHTML+=`<li class="list-group-item">${user[0]} ${user[1]}</li>`;
+document.getElementById('users').innerHTML+=`<li class="collection-item">${user[0]} ${user[1]}</li>`;
     
 });
