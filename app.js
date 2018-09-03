8a8662079d59b290a1f351b8142cad95b542f198
sajith sajeev <sajithsajeevr@outlook.com>
2018-09-04 05:17:25 +0530

    Comments Added,Seperating UI elements,Removed Console logs

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
+//UI elements
+const messageUI=document.getElementById('message');
+const fname=document.getElementById('fname');
+const lname=document.getElementById('lname');
 
 document.querySelector('form').addEventListener('submit',function(e){
 
-console.log('readyy');
-const fname=document.getElementById('fname').value;
-const lname=document.getElementById('lname').value;
-console.log(fname);
+  //Regular expression for validation
 const re=/^[a-zA-Z]{2,10}$/;
-if(!re.test(fname)||!re.test(lname)){
-  console.log("invalidddddddd");
-document.getElementById('message').innerHTML="Input must be Alphabetic & between 2-15 Characters";
+
+//Validation and persisting
+if(!re.test(fname.value)||!re.test(lname.value)){
+  messageUI.innerHTML="Input must be Alphabetic & between 2-15 Characters";
 }else{
-  console.log("validddddddd");
-let name=`${fname}+${lname}`;
+let name=`${fname.value}+${lname.value}`;
 let persistingusersls;
 if(localStorage.getItem('persistingusersls')==null){
   persistingusersls=[];
- console.log(persistingusersls);}
- else{
+}else{
   persistingusersls=JSON.parse(localStorage.getItem('persistingusersls'));
 }
- console.log(persistingusersls)
  persistingusersls.push(name);
  localStorage.setItem('persistingusersls',JSON.stringify(persistingusersls));
- document.getElementById('message').innerHTML="Saved!!!";
+ messageUI.innerHTML="Saved!!!";
 }
 e.preventDefault();
 })
--- usersapp.js
+++ usersapp.js
@@ -1,22 +1,23 @@
+// UI Elements
+const userUI=document.getElementById('users');
 
-
+//Retrievimg data from LS
 if(localStorage.getItem('persistingusersls')==null)
 {
-    document.getElementById('users').innerHTML+=`<li class="collection-item">No Users Found</li>`;
+    userUI.innerHTML+=`<li class="collection-item">No Users Found</li>`;
 }else{
 let users=JSON.parse(localStorage.getItem('persistingusersls'));
-console.log(users);
 users.forEach(element => {
 let user=element.split('+');
-console.log(user[0]);
-document.getElementById('users').innerHTML+=`<li class="collection-item">${user[0]} ${user[1]}</li>`;
+userUI.innerHTML+=`<li class="collection-item">${user[0]} ${user[1]}</li>`;
     
 });}
 
+// To Clear persisted Users 
 document.getElementById('clearUsers').addEventListener('click',function(e){
 localStorage.clear();
-console.log("clear");
+
+//Display Message
 if(localStorage.getItem('persistingusersls')==null){
-    console.log("empty");
-    document.getElementById('users').innerHTML=`<li class="collection-item">No Users Found</li>`;
+    userUI.innerHTML=`<li class="collection-item">No Users Found</li>`;
 }else{}});
