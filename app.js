015271632e095fea9b7ecfc3d56538c1957165c8
sajith sajeev <sajithsajeevr@outlook.com>
2018-09-04 06:38:40 +0530

    added validation message for input fields

--- app.js
+++ app.js
@@ -2,16 +2,23 @@
 const messageUI=document.getElementById('message');
 const fname=document.getElementById('fname');
 const lname=document.getElementById('lname');
+const form=document.querySelector('form');
 
-document.querySelector('form').addEventListener('submit',function(e){
-
-  //Regular expression for validation
+//Regular Expression for validation
 const re=/^[a-zA-Z]{2,10}$/;
 
-//Validation and persisting
-if(!re.test(fname.value)||!re.test(lname.value)){
-  messageUI.innerHTML="Input must be Alphabetic & between 2-15 Characters";
-}else{
+//Event listeners
+form.addEventListener('submit',addUser);
+fname.addEventListener('keyup',fNameValidator);
+lname.addEventListener('keyup',lNameValidator);
+
+//Add User
+function addUser(e){
+
+  //Validation
+  if(!re.test(fname.value)||!re.test(lname.value)){
+    messageUI.innerHTML="Input must be Alphabetic & between 2-15 Characters";
+  }else{
 let name=`${fname.value}+${lname.value}`;
 let persistingusersls;
 if(localStorage.getItem('persistingusersls')==null){
@@ -22,6 +29,23 @@ if(localStorage.getItem('persistingusersls')==null){
  persistingusersls.push(name);
  localStorage.setItem('persistingusersls',JSON.stringify(persistingusersls));
  messageUI.innerHTML="Saved!!!";
-}
+  }
 e.preventDefault();
-})
+}
+
+//First name Validation Message
+function fNameValidator(e){
+  if(!re.test(e.target.value)){
+    messageUI.innerHTML="Input must be Alphabetic & between 2-15 Characters";
+  }else{
+    messageUI.innerHTML="Acceptable First Name";
+  }
+}
+//Last Name Validation Message
+function lNameValidator(e){
+  if(!re.test(e.target.value)){
+    messageUI.innerHTML="Input must be Alphabetic & between 2-15 Characters";
+  }else{
+    messageUI.innerHTML="Acceptable Last Name";
+  }
+}
--- usersapp.js
+++ usersapp.js
@@ -1,5 +1,9 @@
 // UI Elements
 const userUI=document.getElementById('users');
+const clearUserUI=document.getElementById('clearUsers');
+
+//Clear Users Event Listener
+clearUserUI.addEventListener('click',clearUsers);
 
 //Retrievimg data from LS
 if(localStorage.getItem('persistingusersls')==null)
@@ -13,11 +17,19 @@ userUI.innerHTML+=`<li class="collection-item">${user[0]} ${user[1]}</li>`;
     
 });}
 
-// To Clear persisted Users 
-document.getElementById('clearUsers').addEventListener('click',function(e){
+//Clear User Function 
+function clearUsers(e){
+
+//confirmation to clear
+    if(confirm('Are you Sure')){
 localStorage.clear();
+}else{}
 
 //Display Message
 if(localStorage.getItem('persistingusersls')==null){
     userUI.innerHTML=`<li class="collection-item">No Users Found</li>`;
-}else{}});
+}else{}};
+
+
+//'<i class="fa fa-remove"></i>'
+//seperated event listeners added some comments
